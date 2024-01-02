6a4efcb28de900ddf0fb04031b4960d002c06fcf
Ankur Bhadania <abbhadania@gmail.com>
2024-01-02 16:07:37 +0530

    remove extra word from title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -165,7 +165,7 @@ export default function Home() {
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl lg:mx-0">
               <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Hunting Coder111
+              Hunting Coder
               </h2>
               <p className="mt-2 text-lg leading-8 text-gray-600">
               A blog for hunting coders by a hunting coder
